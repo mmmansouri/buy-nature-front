b8d3cab414ace417c1fd2d459036a8a3547abf3b
mmmansouri <mansourimedmoez@gmail.com>
2024-09-08 21:55:32 +0200

    Routage, Home, Header

--- buy-nature-front/src/app/app.component.ts
+++ buy-nature-front/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- buy-nature-front/src/app/app.routes.ts
+++ buy-nature-front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ItemsComponent } from "./pages/items/items.component";
+import { HomeComponent } from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'items', component: ItemsComponent }
+
+];
