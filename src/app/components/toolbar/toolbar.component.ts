4b8faf5a3565c5167391b6877061a6b7da4c0261
mmmansouri <mansourimedmoez@gmail.com>
2024-11-18 22:05:52 +0100

    Make Mini Cart loaded dynamically in the toolbar with the items in cart

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import { SidenavService } from "../sidenav/sidenav.service";
 import { MatToolbarModule} from "@angular/material/toolbar";
 import { MatIconModule} from "@angular/material/icon";
@@ -9,6 +9,7 @@ import {CartService} from "../../pages/cart/cart.service";
 import {CommonModule} from "@angular/common";
 import {ClickOutsideDirective} from "../../directives/click-outside.directive";
 import {ItemInCart} from "../../models/item.in.cart.model";
+import {CartMiniComponent} from "../../pages/cart/cart-mini/cart-mini.component";
 
 @Component({
   selector: 'app-toolbar',
@@ -17,23 +18,62 @@ import {ItemInCart} from "../../models/item.in.cart.model";
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, AfterViewInit {
+  @ViewChild('miniCartContainer', { read: ViewContainerRef }) miniCartContainer!: ViewContainerRef;
+
   cartItems: ItemInCart[] = [];
   numberOfItemsInCart = 0;
 
   cartOpen: boolean = false;
 
+  isItemLoaded = false; // Tracks when the item data is loaded
+  isViewInitialized = false; // Tracks when the ViewChild references are initialized
+
+  private cartMiniComponentInstance: CartMiniComponent | null = null; // Hold the instance of CartMiniComponent
+
   constructor(private sidenavService: SidenavService,private cartService: CartService) {}
 
   ngOnInit() {
-    this.cartService.cartState$.subscribe(cart => {
-      this.cartItems = cart;
-      this.numberOfItemsInCart = this.cartItems.length;
-      console.log('Cart updated:', cart);
+    this.cartService.cartState$.subscribe({
+      next: (cart) => {
+
+        this.cartItems = cart;
+        this.numberOfItemsInCart = this.cartItems.length;
+        this.isItemLoaded = true; // Mark item as loaded
+        this.tryLoadComponents(); // Attempt to load components
+
+        console.log('Cart updated:', cart);
+      },
+      error: (err) => console.error('Error fetching cart:', err)
     })
   }
 
+  // Method to dynamically load components into the grid
+  private tryLoadComponents() {
+    if (this.isItemLoaded && this.isViewInitialized) {
+      // Only proceed if both the item and view references are ready
+      this.loadComponents();
+    }
+  }
 
+  ngAfterViewInit() {
+    this.isViewInitialized = true; // Mark view as initialized
+    this.tryLoadComponents(); // Attempt to load components
+  }
+
+  private loadComponents() {
+    this.miniCartContainer.clear();
+
+    const cartMiniComponentRef = this.miniCartContainer.createComponent(CartMiniComponent);
+    this.cartMiniComponentInstance = cartMiniComponentRef.instance;
+    this.cartMiniComponentInstance.cartItems = this.cartItems;
+    this.cartMiniComponentInstance.cartOpen = this.cartOpen;
+
+    // Subscribe to cartOpenChange event
+    this.cartMiniComponentInstance.cartOpenChange.subscribe((newCartOpen: boolean) => {
+      this.cartOpen = newCartOpen; // Update the toolbar's cartOpen field
+    });
+  }
 
 
   toggleMenu() {
@@ -43,10 +83,9 @@ export class ToolbarComponent implements OnInit {
   toggleCartShow(event: MouseEvent) {
     event.stopPropagation();
     this.cartOpen = !this.cartOpen;
-  }
-
-  closeCart() {
-    this.cartOpen = false;
+    if (this.cartMiniComponentInstance) {
+      this.cartMiniComponentInstance.cartOpen = this.cartOpen;
+    }
   }
 
 }
--- src/app/pages/cart/cart-mini/cart-mini.component.ts
+++ src/app/pages/cart/cart-mini/cart-mini.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {SidenavService} from "../../../components/sidenav/sidenav.service";
+import {CartService} from "../cart.service";
+import {ItemInCart} from "../../../models/item.in.cart.model";
+import {ClickOutsideDirective} from "../../../directives/click-outside.directive";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-cart-mini',
   standalone: true,
-  imports: [],
+  imports: [
+    ClickOutsideDirective,
+    NgIf
+  ],
   templateUrl: './cart-mini.component.html',
   styleUrl: './cart-mini.component.scss'
 })
-export class CartMiniComponent {
+export class CartMiniComponent implements OnInit {
+
+  @Input()
+  cartItems: ItemInCart[] = [];
+
+  @Input()
+  cartOpen: boolean = false;
+
+  @Output() cartOpenChange = new EventEmitter<boolean>();
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartService.cartState$.subscribe(cart => {
+      this.cartItems = cart;
+    })
+  }
+
+  closeCart() {
+    this.cartOpen = false;
+    this.cartOpenChange.emit(this.cartOpen);
+  }
 
 }
