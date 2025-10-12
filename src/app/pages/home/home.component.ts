156b7dfcc8594ddb0e096c6cecc43fe009b87845
mmmansouri <mansourimedmoez@gmail.com>
2025-10-12 03:00:46 +0200

    Refactor styles and layout for home, cart, and checkout components to implement bio theme, enhance responsiveness, and improve UI consistency

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        RouterLink
+    ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
